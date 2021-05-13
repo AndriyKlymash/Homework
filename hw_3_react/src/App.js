5cc5a38ebfad28bdabde6e1cade5fda0c5160fb6
AndriyKlymash <andriyklymash2020@gmail.com>
2021-05-13 18:22:03 +0300

    hw_4_react

--- hw_3_react/src/App.js
+++ hw_3_react/src/App.js
@@ -5,17 +5,33 @@
 // В компоненті поста повинна бути кнопка, при кліку на яку поруч зі списком всіх постів з'являється обраний пост і його деталі.
 
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import Users from "./components/users/users";
 import Posts from "./components/posts/Posts";
 
 function App() {
-  return (
-    <div className="App">
-          <Users/>
-        <Posts/>
+    return (
+        <Router>
+            <div className="App">
+                <Link to={'users'}>to users</Link>
+                <br/>
+                <Link to={'posts'}>to posts</Link>
+                <Switch>
+                    <Route path={'/users'} render={() => <Users/>}/>
+                    <Route path={'/posts'} render={(props) => {
+                        console.log(props)
+                        return <Posts/>
+                    }}/>
+                </Switch>
 
-    </div>
-  );
+            </div>
+        </Router>
+    );
 }
 
 export default App;

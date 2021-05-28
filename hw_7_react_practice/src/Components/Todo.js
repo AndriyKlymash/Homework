1ba8e6cdbb5c9a711086d34f86c191fb1b858495
AndriyKlymash <andriyklymash2020@gmail.com>
2021-05-28 21:12:44 +0300

    clean app

--- hw_7_react_practice/src/Components/Todo.js
+++
@@ -1,8 +0,0 @@
-export default function Todo({todo: {todoItem, id}, idX, deleteTodo}) {
-    console.log(todoItem.id);
-    return(
-        <h4>
-            {idX}. {todoItem} <button onClick={() => deleteTodo(id)}>Delete</button>
-        </h4>
-    );
-}
